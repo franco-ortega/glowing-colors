b9ab47416f4e75866bd9c0ced3c9c178f99e48b4
Franco Ortega <francopdx@gmail.com>
2021-08-22 22:10:01 -0700

    create colors dynamically on render

--- src/components/Orb/Orb.jsx
+++ src/components/Orb/Orb.jsx
@@ -3,10 +3,33 @@ import React from 'react';
 import styles from './Orb.module.css';
 
 const Orb = () => {
+
+  const colorCode = () => {
+    return Math.floor(Math.random() * 256);
+  };
+
+  const colorPicker = () => {
+    const red = colorCode();
+    const green = colorCode();
+    const blue = colorCode();
+
+    return `rgb(${red}, ${green}, ${blue})`;
+  };
+
+  const bkdgColor = colorPicker();
+  const borderStyle = `thick solid ${colorPicker()}`;
+
+  console.log(bkdgColor);
+  console.log(borderStyle);
+
   return (
     <div
       className={styles.Orb}
       data-testid="orb-element"
+      style={{
+        background: bkdgColor,
+        border: borderStyle
+      }}
     ></div>
   );
 };
