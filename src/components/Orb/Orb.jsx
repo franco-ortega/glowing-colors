4ab04b8c6ab6d6e15b1ad730caa37750b0b365de
Franco Ortega <francopdx@gmail.com>
2021-08-22 22:21:06 -0700

    add opacity to colorPicker

--- src/components/Orb/Orb.jsx
+++ src/components/Orb/Orb.jsx
@@ -8,17 +8,17 @@ const Orb = () => {
     return Math.floor(Math.random() * 256);
   };
 
-  const colorPicker = () => {
+  const colorPicker = (opacity) => {
     const red = colorCode();
     const green = colorCode();
     const blue = colorCode();
 
-    return `rgb(${red}, ${green}, ${blue})`;
+    return `rgb(${red}, ${green}, ${blue}, ${opacity})`;
   };
 
-  const bkdgColor = colorPicker();
-  const borderStyle = `thick solid ${colorPicker()}`;
-  const boxShadowStyle = `0 0 2vw 2vw ${colorPicker()}`;
+  const bkdgColor = colorPicker(.75);
+  const borderStyle = `medium solid ${colorPicker(.5)}`;
+  const boxShadowStyle = `0 0 2vw 2vw ${colorPicker(1)}`;
 
   console.log(bkdgColor);
   console.log(borderStyle);
