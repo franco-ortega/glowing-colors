e35c6a350aeff9582066d566c82021b79184135b
Franco Ortega <francopdx@gmail.com>
2021-08-22 21:13:12 -0700

    add test to render Orb

---
+++ src/components/Orb/Orb.test.js
@@ -0,0 +1,14 @@
+import { render, screen } from "@testing-library/react";
+import Orb from './Orb';
+
+describe('Orb tests', () => {
+    it('should render an Orb', () => {
+        render(
+            <Orb />
+        );
+
+        const element = screen.getByTestId('orb-element');
+
+        expect(element).not.toBeEmptyDOMElement();
+    });
+});
