5b562e77bfc761d36fff61c039da0562dacb5f35
Franco Ortega <francopdx@gmail.com>
2021-08-23 14:49:19 -0700

    add test for colorCode to return an integer between 0 and 255

--- src/utils/utils.test.js
+++ src/utils/utils.test.js
@@ -2,7 +2,7 @@ import { colorCode } from './utils';
 
 describe('test for utils functions', () => {
 
-  describe('returns an integer between 0 and 255', () => {
+  describe('return an integer between 0 and 255', () => {
     test('return a number', () => {
       const result = colorCode();
       const expected = (typeof result === 'number');
@@ -11,6 +11,12 @@ describe('test for utils functions', () => {
       expect(expected).toBe(true);
     });
 
+    test('return an integer between 0 and 255', () => {
+      const result = colorCode();
+      const expected = (0 <= result <= 255);
+
+      expect(expected).toBe(true);
+    });
 
   });
 });
