e8f7ea7706210b01138c69deef22b1e140893e8c
Franco Ortega <francopdx@gmail.com>
2021-08-27 21:11:31 -0700

    add onOrbClick and transition

--- src/components/Orb/Orb.jsx
+++ src/components/Orb/Orb.jsx
@@ -1,17 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { generateOrbStyle } from '../../utils/utils';
 import styles from './Orb.module.css';
 
 const Orb = () => {
+  const [move, setMove] = useState(false);
+
   const orbStyle = generateOrbStyle();
 
   const onOrbClick = () => {
-    console.log('Orb clicked.');
+    console.log('Orb clicked: ' + move);
+    setMove(!move);
   };
+
+  console.log(move);
 
   return (
     <button
-      className={styles.Orb}
+      className={`${styles.Orb}`}
       data-testid="orb-element"
       style={orbStyle}
       onClick={onOrbClick}
