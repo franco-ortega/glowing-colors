0ce17cd9be8f08ab255c52883f59cf4acd9a1d3f
Franco Ortega <francopdx@gmail.com>
2021-08-30 21:32:11 -0700

    add test for Welcome component

---
+++ src/components/welcome/Welcome.test.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Welcome from './Welcome';
+
+describe('Welcome tests', () => {
+  test('should render the Welcome component', () => {
+    render(
+      <Welcome />
+    );
+
+    const element = screen.getByText('Glowing Colors');
+
+    expect(element).toContain('Glowing Colors');
+  });
+});
