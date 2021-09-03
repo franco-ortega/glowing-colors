1c82e57fdc86239b99732c7857dd7c20d7a3105a
Franco Ortega <francopdx@gmail.com>
2021-09-03 09:50:10 -0700

    update copy

--- src/components/welcome/Welcome.jsx
+++ src/components/welcome/Welcome.jsx
@@ -6,7 +6,8 @@ const Welcome = ({ onBeginClick }) => {
   return (
     <section className={styles.Welcome}>
       <h1>Glowing Colors</h1>
-      <p>Click on the orbs to change their size and colors.</p>
+      {/* eslint-disable-next-line max-len */}
+      <p>Welcome to this world of softly changing colors. When you click the Begin button, colorful orbs will appear and fade away with new sizes and colors each time. Also, you can click on the orbs to move them while their sizes and colors change.</p>
       <button onClick={onBeginClick}>Continue</button>
     </section>
   );
